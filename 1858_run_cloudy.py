fd5daa952687415486a426ddbe3beaf96d1719b0
jhmatthews <matthews@ast.cam.ac.uk>
2023-03-15 09:33:16 +0000

    removed data, added plots, tweaks to plotting and running script to make more versatile

--- 1858_run_cloudy.py
+++ 1858_run_cloudy.py
@@ -3,28 +3,27 @@ import sys
 import numpy as np
 import time
 import convert_ip
+from tqdm import tqdm
 
 # some constants 
 # CAUTION: DENSITY IS HARDWIRED AT THE MOMENT
 BOLTZMANN = 1.38062e-16
 EV2ERGS   = 1.602192e-12
-LOG_N = 9 
 COLUMN = 23
 OUTPUT_FOLDER = "cld_output"
 
 CNO_STRING = """
-abundances solar
+abundances H II region
 element scale factor helium 2
 element scale factor carbon 0.5
-element scale factor nitrogen 7
-"""
+element scale factor nitrogen 7"""
 
 
-def run_cloudy(fname, cloudy_path="/Users/matthewsj/software/c17.01/source/"):
-	isys = os.system("{}/cloudy.exe < {}.in > {}.out".format(cloudy_path, fname, fname))
+def run_cloudy(fname, cloudy_path="/Users/matthewsj/software/c17.01/source/", folder="cld_data/"):
+	isys = os.system("{}/cloudy.exe < {}/{}.in > {}/{}.out".format(cloudy_path, folder, fname, folder, fname))
 	return (isys)
 
-def get_param_string(root, log_xi, log_U, BB_temp = 580225.906078, abundance="solar"):
+def get_param_string(root, log_xi, log_U, BB_temp = 580225.906078, abundance="solar", LOG_N = 9):
 	'''
 	Create a string to write to a cloudy input file. 
 
@@ -58,8 +57,7 @@ ionization parameter {:.1f}
 # =========
 # commands for density & abundances
 # =========
-hden {:.1f}
-{}
+hden {:.1f}{}
 # =========
 # commands controlling geometry  
 # =========
@@ -81,7 +79,7 @@ element limit off -8
 # commands controlling output    =========
 # =========
 normalize to "H  1" 1215.67A 100 
-print line faint 1  # get rid of lots of faint lines c
+# print line faint 1  # get rid of lots of faint lines c
 # print off
 print lines column
 save lines, array "{}.lines" last
@@ -91,7 +89,7 @@ save continuum "{}.cont"
 '''.format(LOG_N, log_xi, BB_temp, log_U, LOG_N, abundance_string, root, root)
 	return my_string
 
-def initialise_cloudy_sim(root, log_xi, log_U, BB_temp = 580225.906078, abundance="solar"):
+def initialise_cloudy_sim(root, log_xi, log_U, LOG_N = 9, BB_temp = 580225.906078, abundance="solar", folder="cld_data/"):
 	'''
 	Initialise a cloudy simulation 
 
@@ -108,21 +106,21 @@ def initialise_cloudy_sim(root, log_xi, log_U, BB_temp = 580225.906078, abundanc
 		BB_temp float 
 				Blackbody temperature in K (linear)
 
-	Returns:
-		my_string str
-		string containing text to use for input file
+		folder  str
+				folder to store data (default cld_data/)
+
 	'''
 
-	# get the string to write to file
-	params = get_param_string(root, log_xi, log_U, BB_temp = BB_temp, abundance=abundance)
+	# get the string to write to file
+	params = get_param_string(root, log_xi, log_U, LOG_N=LOG_N, BB_temp = BB_temp, abundance=abundance)
 	
-	# chuck the text in an input file 
-	fname = "{}.in".format(root)
+	# chuck the text in an input file 
+	fname = "{}/{}.in".format(folder, root)
 	f = open(fname, "w")
 	f.write(params)
 	f.close()
 
-def run_logxi_grid(logxis =  np.arange(-3,2,0.5), BB_TEMP = 50.0, abundance="solar"):
+def run_logxi_grid(logxis =  np.arange(-3,2,0.5), BB_TEMP = 50.0, LOG_N = 9, abundance="solar", folder="cld_data/"):
 	'''
 	Run a grid of blackbody cloudy simulations 
 
@@ -134,30 +132,37 @@ def run_logxi_grid(logxis =  np.arange(-3,2,0.5), BB_TEMP = 50.0, abundance="sol
 				Blackbody temperature in ELECTRON VOLTS (linear)
 	'''
 
-	for i, logxi in enumerate(logxis):
-		print (i, logxi)
+	for i, logxi in enumerate(tqdm(logxis)):
+		#print (i, logxi)
 
 		# we need to calculate U, the IP used by Cloudy. 
 		xi = 10.0 ** logxi 
-		factor = convert_ip.get_conversion_factor_blackbody(temp_eV = 50.0)
+		factor = convert_ip.get_conversion_factor_blackbody(temp_eV = BB_TEMP)
 		U = xi * factor 
 		logU = np.log10(U)
 
 		# root filename to use for input and output files
-		root = "BB_{:.1f}_1858_n{:.1f}_xi{:.1f}".format(BB_TEMP, LOG_N, logxi)
+		root = "BB_{:.1f}_1858_n{:.1f}_xi{:.3f}".format(BB_TEMP, LOG_N, logxi)
 
 		if abundance == "cno":
 			root += abundance
 
-		print (BB_TEMP * EV2ERGS / BOLTZMANN)
+		#print (BB_TEMP * EV2ERGS / BOLTZMANN)
 
 		# Initialise simulation by creating input file 
-		initialise_cloudy_sim(root, logxi, logU, BB_temp = BB_TEMP * EV2ERGS / BOLTZMANN, abundance=abundance)
+		initialise_cloudy_sim(root, logxi, logU, LOG_N = LOG_N, BB_temp = BB_TEMP * EV2ERGS / BOLTZMANN, abundance=abundance, folder=folder)
 
 		# run the sim! 
-		run_cloudy(root)
+		run_cloudy(root, folder=folder)
 
 if __name__ == "__main__":
+	BB_TEMP = float(sys.argv[1])
+	abundance = str(sys.argv[2])
+
+	run_logxi_grid(logxis =  np.arange(-3,2,0.125), BB_TEMP = BB_TEMP, LOG_N = 9, abundance=abundance)
+
 	#run_logxi_grid(logxis =  np.arange(-3,2,0.5), BB_TEMP = 50.0, abundance="solar")
-	run_logxi_grid(logxis =  np.arange(-3,2,0.5), BB_TEMP = 50.0, abundance="cno")
+	#run_logxi_grid(logxis =  np.arange(-3,2,0.25), BB_TEMP = 10.0, abundance="solar")
+	#run_logxi_grid(logxis =  np.arange(-3,2,0.25), BB_TEMP = 50.0, abundance="cno")
+	#run_logxi_grid(logxis =  np.arange(-3,2,0.25), BB_TEMP = 10.0, abundance="cno")
 
--- plot_ratios.py
+++ plot_ratios.py
@@ -1,70 +1,183 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import jm_util
+jm_util.set_mod_defaults()
+
+o4blends = ["1397.20A", "1399.77A", "1401.16A", "1404.78A", "1407.38A"]
+nvblends = ["1239.93A", "1240.40A"]
+# do you want to include Mg II and OIV lines for NV and Si IV as Mauche does
+INCLUDE_BLENDS = True
+plot_without_blends_too = True
 
 
+def get_cloudy_line_strengths(BB_TEMP, LOG_N, logxi, folder="cld_data/", suffix="",
+                              line_names=["c4", "n5", "he2", "si4", "o4", "o4sum", "mg2sum", "si4_raw", "n5_raw"]):
+    """Get cloudy line strengths from a given file for a range of IPs
 
-logxis = np.arange(-3,1.5,0.5)
+    Args:
+            BB_TEMP (float, optional): black body temperature in eV. 
+            LOG_N (float, optional): log electron density. 
+            logxi (array-like): logarithms of ionization parameters
+            folder (str, optional): data folder name. Defaults to "cld_data/".
+            suffix (str, optional): CNO or not? ==cno if so, blank otherwise
+            line_names (list, optional): list of line names. Defaults to ["c4", "n5","he2", "si4"].
 
-# BB_TEMP in electron volts
-BB_TEMP = 50.0
-LOG_N = 9
+    Returns:
+            cld_data (dict): dictionary of cloudy data
+    """
+    cld_data = dict()
 
-# create arrays to hold line luminosities
-c4 = np.zeros_like(logxis)
-si4 = np.zeros_like(logxis)
-n5 =  np.zeros_like(logxis)
-he2 =  np.zeros_like(logxis)
+    # create arrays to hold line luminosities
+    for l in line_names:
+        cld_data[l] = np.zeros_like(logxi)
 
-for i, logxi in enumerate(logxis):
-	root = "BB_{:.1f}_1858_n{:.1f}_xi{:.1f}".format(BB_TEMP, LOG_N, logxi)
-	f = open(root + ".lines", "r")
-	for line in f:
-		data = line.split()
-		if data[1] == "Blnd" and data[2] == "1397.00A":
-			si4[i] = float(data[4]) 
-		if data[1] == "Blnd" and data[2] == "1549.00A":
-			c4[i] = float(data[4]) 
-		if data[1] == "Blnd" and data[2] == "1240.00A":
-			n5[i] = float(data[4])
-		if data[1] == "He" and data[2] == "2" and data[3] == "1640.43A":
-			he2[i] = float(data[5]) 
+    for i, logxi in enumerate(logxis):
+        try:
+            root = root = "{}/BB_{:.1f}_1858_n{:.1f}_xi{:.3f}{}".format(
+                folder, BB_TEMP, LOG_N, logxi, suffix)
+            f = open(root + ".lines", "r")
+            for line in f:
+                data = line.split()
+                if data[1] == "Blnd" and data[2] == "1397.00A":
+                    cld_data["si4"][i] = 10.0**float(data[4])
+                if data[1] == "Blnd" and data[2] == "1549.00A":
+                    cld_data["c4"][i] = 10.0**float(data[4])
+                if data[1] == "Blnd" and data[2] == "1240.00A":
+                    cld_data["n5"][i] = 10.0**float(data[4])
+                if data[1] == "He" and data[2] == "2" and data[3] == "1640.43A":
+                    cld_data["he2"][i] = 10.0**float(data[5])
+                if data[1] == "Blnd" and data[2] == "1402.00A":
+                    cld_data["o4"][i] = 10.0**float(data[4])
 
-# array used to plot a 1:1 line 
-logratios = np.linspace(-2.5,1,100)
+                # do you want to include Mg II and O IV blended lines as contributions?
+                if INCLUDE_BLENDS:
+                    if data[1] == "Mg" and data[2] == "2" and data[3] in nvblends:
+                        cld_data["mg2sum"][i] += 10.0**float(data[5])
+                    if data[1] == "O" and data[2] == "4" and data[3] in o4blends:
+                        cld_data["o4sum"][i] += 10.0**float(data[5])
 
-# read in the Mauche data for comparison 
-lxi_mauche, si4ratio_mauche, n5ratio_mauche, he2ratio_mauche = np.genfromtxt("mauche_data_50eV.dat", unpack=True)
+        except FileNotFoundError:
+            print("no file for log xi of ", logxi)
 
-# make some figures 
-plt.figure()
-plt.title("Model C")
-plt.plot(logxis,si4-c4, "-o", label="Cloudy",)
-plt.plot(lxi_mauche,si4ratio_mauche,"-x", label = "Mauche")
-plt.xlabel(r"$\log \xi$",fontsize=18)
-plt.ylabel(r"$\log ({\rm Si IV / CIV})$",fontsize=18)
-plt.legend()
-plt.savefig("mauche_cld_comp_si4c4.png", dpi=300)
+        # store lines without the blends
+        cld_data["n5_raw"][i] = cld_data["n5"][i]
+        cld_data["si4_raw"][i] = cld_data["si4"][i]
 
-plt.figure()
-plt.title("Model C")
-plt.plot(si4-c4,n5-c4, "-o", label="Cloudy",)
-plt.plot(si4ratio_mauche,n5ratio_mauche,"-x", label = "Mauche")
-plt.ylabel(r"$\log ({\rm N V / CIV})$",fontsize=18)
-plt.xlabel(r"$\log ({\rm Si IV / CIV})$",fontsize=18)
-plt.plot(logratios,logratios, ls="--", c="k")
-plt.legend()
-plt.xlim(-2.5,1)
-plt.ylim(-2.5,2)
-plt.savefig("mauche_cld_comp_ratio1.png", dpi=300)
+        # include blended lines a la Mauche
+        cld_data["si4"][i] += cld_data["o4sum"][i]
+        cld_data["n5"][i] += cld_data["mg2sum"][i]
 
-plt.figure()
-plt.title("Model C")
-plt.plot(si4-c4,he2-c4, "-o", label="Cloudy",)
-plt.plot(si4ratio_mauche,he2ratio_mauche,"-x", label = "Mauche")
-plt.plot(logratios,logratios, ls="--", c="k")
-plt.xlim(-2.5,1)
-plt.ylim(-1.5,1)
-plt.ylabel(r"$\log ({\rm He II / CIV})$",fontsize=18)
-plt.xlabel(r"$\log ({\rm Si IV / CIV})$",fontsize=18)
-plt.legend()
-plt.savefig("mauche_cld_comp_ratio2.png", dpi=300)
+        # store them as log quantities
+        for l in line_names:
+            cld_data[l][i] = np.log10(cld_data[l][i])
+
+        print(root, cld_data["o4"][i], cld_data["o4sum"]
+              [i], cld_data["n5"][i], cld_data["c4"][i])
+        # if
+        f.close()
+
+    return (cld_data)
+
+
+def make_figure(logxis, folder="cld_data/", BB_TEMP=50.0, LOG_N=9.0):
+    """Make the figures for comparison with Mauche+ 1997
+
+    Args:
+            logxis (_type_): log ionization parameters
+            folder (str, optional): folder that contains the cloudy data. Defaults to "cld_data/".
+            BB_TEMP (float, optional): black body temperature in eV. Defaults to 50.0.
+            LOG_N (float, optional): log electron density. Defaults to 9.0.
+    """
+
+    # get data
+    cld_data = get_cloudy_line_strengths(BB_TEMP, LOG_N, logxis, folder=folder)
+    cld_data_cno = get_cloudy_line_strengths(
+        BB_TEMP, LOG_N, logxis, folder=folder, suffix="cno")
+
+    # array used to plot a 1:1 line
+    logratios = np.linspace(-4, 4, 100)
+
+    # read in the Mauche data for comparison
+    lxi_mauche, si4ratio_mauche, n5ratio_mauche, he2ratio_mauche = np.genfromtxt(
+        "mauche_data/mauche_data_{:.0f}eV.dat".format(BB_TEMP), unpack=True)
+
+    # kwargs for plotting
+    mauche_kwargs = {"marker": "x", "linestyle": "--",
+                     "color": "C1", "label": "Mauche"}
+    cno_kwargs = {"marker": None, "linestyle": "-",
+                  "color": "C2", "label": "Cloudy CNO", "lw": 3}
+    raw_kwargs = {"marker": None, "linestyle": "-", "alpha": 0.3,
+                  "color": "C2", "label": "Cloudy CNO (no blends)", "lw": 4}
+    std_kwargs = {"marker": None, "linestyle": "-",
+                  "color": "C0", "label": "Cloudy", "lw": 3}
+    save_kwargs = {"dpi": 300}
+
+    sed_string = "bb{:.0f}".format(BB_TEMP)
+    title_string = "Blackbody, {:.0f} eV".format(BB_TEMP)
+
+    # make some figures
+    plt.figure()
+    plt.title(title_string)
+    plt.plot(logxis, cld_data["si4"]-cld_data["c4"], **std_kwargs)
+    plt.plot(logxis, cld_data_cno["si4"]-cld_data_cno["c4"], **cno_kwargs)
+
+    if plot_without_blends_too:
+        plt.plot(logxis, cld_data_cno["si4_raw"]-cld_data_cno["c4"], **raw_kwargs)
+    plt.plot(lxi_mauche, si4ratio_mauche, **mauche_kwargs)
+    plt.xlabel(r"$\log \xi$", fontsize=18)
+    plt.ylabel(r"$\log ({\rm Si IV / CIV})$", fontsize=18)
+    plt.legend()
+    plt.tight_layout()
+    # plt.xlim(-3,2)
+    plt.ylim(-3,2)
+    plt.savefig("mauche_cld_comp_si4c4_{}.png".format(
+        sed_string), **save_kwargs)
+
+    plt.figure()
+    plt.title(title_string)
+    plt.plot(cld_data["si4"]-cld_data["c4"],
+             cld_data["n5"]-cld_data["c4"], **std_kwargs)
+    plt.plot(cld_data_cno["si4"]-cld_data_cno["c4"],
+             cld_data_cno["n5"]-cld_data_cno["c4"], **cno_kwargs)
+    if plot_without_blends_too:
+        plt.plot(cld_data_cno["si4_raw"]-cld_data_cno["c4"],
+             cld_data_cno["n5_raw"]-cld_data_cno["c4"], **raw_kwargs)
+    plt.plot(si4ratio_mauche, n5ratio_mauche, **mauche_kwargs)
+    plt.ylabel(r"$\log ({\rm N V / CIV})$", fontsize=18)
+    plt.xlabel(r"$\log ({\rm Si IV / CIV})$", fontsize=18)
+    plt.plot(logratios, logratios, ls="--", c="k")
+    plt.scatter(1.1, 1.8, c="C3", marker="*", s=200)
+    plt.legend()
+    plt.xlim(-4, 4)
+    plt.ylim(-4, 4)
+    plt.tight_layout()
+    plt.savefig("mauche_cld_comp_ratio1_{}.png".format(
+        sed_string), **save_kwargs)
+
+    plt.figure()
+    plt.title(title_string)
+    plt.plot(cld_data["si4"]-cld_data["c4"],
+             cld_data["he2"]-cld_data["c4"], **std_kwargs)
+    plt.plot(cld_data_cno["si4"]-cld_data_cno["c4"],
+             cld_data_cno["he2"]-cld_data_cno["c4"], **cno_kwargs)
+    if plot_without_blends_too:
+        plt.plot(cld_data_cno["si4_raw"]-cld_data_cno["c4"],
+             cld_data_cno["he2"]-cld_data_cno["c4"], **raw_kwargs)
+    plt.plot(si4ratio_mauche, he2ratio_mauche, **mauche_kwargs)
+    plt.plot(logratios, logratios, ls="--", c="k")
+    plt.xlim(-4, 4)
+    plt.ylim(-4, 4)
+    plt.scatter(1.1, 1.1, c="C3", marker="*", s=200)
+    plt.ylabel(r"$\log ({\rm He II / CIV})$", fontsize=18)
+    plt.xlabel(r"$\log ({\rm Si IV / CIV})$", fontsize=18)
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig("mauche_cld_comp_ratio2_{}.png".format(
+        sed_string), **save_kwargs)
+
+
+if __name__ == "__main__":
+    logxis = np.arange(-3, 2, 0.125)
+    make_figure(logxis, BB_TEMP=10.0)
+    make_figure(logxis, BB_TEMP=30.0)
+    make_figure(logxis, BB_TEMP=50.0)
